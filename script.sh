714cd58894eb155a5fcf39c7c00e7b8cb82f22f7
ISF <ISF786@proton.me>
2025-05-16 22:08:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,20 +2,31 @@
 #shebang                                                                                                                                                                                                                                                                  echo "Installing & Updating Requirements ===>"
 
 echo "Installing & Upadating Softwares[+]"
-sleep 1
-
+sleep 2
 apt-get install vim -y; apt install openssh-sever -y;
 apt-get install python3 -y; apt install vlc -y;                                                                                          
-apt install git -y/n; apt install dust -y/n;                                                                                                                                                                                                                                   #dust for seeing directory vise instant view
+apt-get install git -y; apt install dust -y/n;                                                                                                                                                                                                                                   #dust for seeing directory vise instant view
 
-#ffmpeg to install  videocompressor                                                                                                  
-#radio-active to listen radio on terminal
-#onefetch => local git repo nfo display
-#ClamAV antivirus
-#Nmap
-#GoTTY =>simple cli tool turn ytu cli tools into web applciation.
-#mitmproxy
+echo "Updating[+]"
+apt-get update
+
+apt-get install ffmpeg 
+#to install  videocompressor                                                                                                  
+apt-get install radio-active 
+#to listen radio on terminal
+apt-get install onefetch 
+#local git repo nfo display
+apt-get install ClamAV 
+#antivirus
+apt-get install nmap
+#Network mapper
+apt-get install GoTTY 
+#simple cli tool turn ytu cli tools into web applciation.
+apt-get install mitmproxy
 #mitmproxy is your swiss-army knife for debugging, testing, privacy measurements, and penetration testing. It can be used to intercet, #inspect, modify and replay web traffic such as HTTP/1, HTTP/2, WebSockets, or any other SSL/TLS-protected protocols
-#yt-dlp =>yt-dlp is software that lets you easily download videos and audio from more than a thousand websites.
-#croc =>croc is a tool that allows any two computers to simply and securely transfer files and folders. End-to-end encryption (using PAKE) is provided.
-#ExifTool => metadata of variety of files.
+apt-get install yt-dlp 
+#yt-dlp is software that lets you easily download videos and audio from more than a thousand websites.
+apt-get install croc 
+#croc is a tool that allows any two computers to simply and securely transfer files and folders. End-to-end encryption (using PAKE) is provided.
+apt-get install ExifTool 
+#metadata of variety of files.
