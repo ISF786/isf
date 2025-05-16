81dfd7598b72f612e3cd75ebb729d4cdee8646db
ISF <ISF786@proton.me>
2025-05-16 22:02:59 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#shebang                                                                                                                                                                                                                                                                  echo "Installing & Updating Requirements ===>"
+
+echo "Installing & Upadating Softwares[+]"
+sleep 1
+
+apt-get install vim -y; apt install openssh-sever -y;
+apt-get install python3 -y; apt install vlc -y;                                                                                          
+apt install git -y/n; apt install dust -y/n;                                                                                                                                                                                                                                   #dust for seeing directory vise instant view
+
+#ffmpeg to install  videocompressor                                                                                                  
+#radio-active to listen radio on terminal
+#onefetch => local git repo nfo display
+#ClamAV antivirus
+#Nmap
+#GoTTY =>simple cli tool turn ytu cli tools into web applciation.
+#mitmproxy
+#mitmproxy is your swiss-army knife for debugging, testing, privacy measurements, and penetration testing. It can be used to intercet, #inspect, modify and replay web traffic such as HTTP/1, HTTP/2, WebSockets, or any other SSL/TLS-protected protocols
+#yt-dlp =>yt-dlp is software that lets you easily download videos and audio from more than a thousand websites.
+#croc =>croc is a tool that allows any two computers to simply and securely transfer files and folders. End-to-end encryption (using PAKE) is provided.
+#ExifTool => metadata of variety of files.
